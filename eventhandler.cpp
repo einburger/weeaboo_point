bf1c703aed786dd104a372687d13bcc40c997d98
einburger <ctobiasc@gmail.com>
2019-11-28 19:24:16 -0600

    simplified event handling

--- eventhandler.cpp
+++ eventhandler.cpp
@@ -12,31 +12,7 @@ void eventhandler_event_create(int ID, void* obj)
 	Event* event = PUSH_OBJECT(&event_stack, Event);
 	event->ID = ID;
 	event->obj = obj;
-
-	switch (ID)
-	{
-	case MOVE:
-		EVENT_MOVE = geometry_box_move;
-		break;
-	case WAIT:
-		EVENT_WAIT = geometry_box_wait;
-		break;
-	case FADE:
-		EVENT_FADE = geometry_box_fade;
-		break;
-	case PARSE:
-		EVENT_PARSE = parse;
-		break;
-	case WRITE:
-		EVENT_WRITE = text_draw;
-		break;
-	case GET_INPUT:
-		EVENT_INPUT = scene_get_input;
-	default:
-		break;
-	}
 }
-
 
 void eventhandler_event_handle()
 {
@@ -55,26 +31,26 @@ void eventhandler_event_handle()
 		switch (event->ID)
 		{
 		case MOVE:
-			if (!EVENT_MOVE(ch, ch->target_pos, ch->y_min, ch->speed))
+			if (!geometry_box_move(ch, ch->target_pos, ch->y_min, ch->speed))
 				move_to_end_and_remove(event);
 			break;
 		case FADE:
-			if (!EVENT_FADE(ch, ch->fade_speed))
+			if (!geometry_box_fade(ch, ch->fade_speed))
 				move_to_end_and_remove(event);
 			break;
 		case WAIT:
-			if (!EVENT_WAIT(ch->wait_time))
+			if (!geometry_box_wait(ch->wait_time))
 				move_to_end_and_remove(event);
 			break;
 		case PARSE:
 			POP_OBJECT(&event_stack, Event);
-			EVENT_PARSE();
+			parse();
 			break;
 		case WRITE:
 			if (game_state->text_cursor_pos < strlen((const char*)event->obj))
 			{
 				game_state->text_cursor_pos++;
-				EVENT_WRITE((const char*)event->obj);
+				text_draw((const char*)event->obj);
 			}
 			else
 			{ // line was written, so wait for input after line is written
@@ -83,22 +59,16 @@ void eventhandler_event_handle()
 			}
 			break;
 		case GET_INPUT:
-			EVENT_INPUT();
+			scene_get_input();
 			break;
 		}
 	}
 }
 
-void eventhandler_event_a_eq_b(Event* a, Event* b)
-{
-	a->ID = b->ID;
-	a->obj = b->obj;
-	a->u = b->u;
-}
-
 void move_to_end_and_remove(Event* e)
 {
 	Event* last = GET_END(&event_stack, Event);
-	eventhandler_event_a_eq_b(e, last);
+	e->ID = last->ID;
+	e->obj = last->obj;
 	POP_OBJECT(&event_stack, Event);
 }
--- eventhandler.h
+++ eventhandler.h
@@ -5,33 +5,12 @@ typedef struct Event
 {
 	int ID;
 	void* obj;
-	union
-	{
-		char(*move_ptr)(Box*, int, int, int);
-		char(*fade_ptr)(Box*, float);
-		char(*wait_ptr)(double);
-		void(*write_ptr)(const char*);
-		void(*parse_ptr)();
-		void(*input_ptr)();
-	} u;
 } Event;
-
-#define EVENT_MOVE event->u.move_ptr
-#define EVENT_FADE event->u.fade_ptr
-#define EVENT_WAIT event->u.wait_ptr
-#define EVENT_WRITE event->u.write_ptr
-#define EVENT_PARSE event->u.parse_ptr
-#define EVENT_INPUT event->u.input_ptr
 
 void eventhandler_event_create(int ID, void* obj);
 void eventhandler_event_handle();
-void eventhandler_event_a_eq_b(Event* a, Event* b);
-
-/* set one event to the other */
-static void eventhandler_event_a_eq_b(Event* a, Event* b);
 
 /* Once an even is handled, swap the end of the stack with it,
  * and decrement stack pointer, effectively removeing it */
 static void move_to_end_and_remove(Event*);
 
-// void   eventhandler_event_handle(Event *event);
